38de437a24c6c450400e0fa9727910103ccf06ee
Cameron Reikes <cameronreikes@gmail.com>
2018-05-20 14:31:40 -0700

    Delete useless files due to git repository

--- autoupdate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Automatically gets the latest autorice script from github
-
-rm autorice.sh
-curl -o autorice.sh https://raw.githubusercontent.com/creikey/autorice/master/autorice.sh
-chmod +x autorice.sh
--- upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Uploads to transfer.sh servers for easy download
-
-curl --upload-file ./autoupdate.sh https://transfer.sh/autoupdate.sh
-echo
